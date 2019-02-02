aa8fecdd1512036807ce1ef36730419f63a96f9e
Greg <g.maceachern@queensu.ca>
2019-02-01 23:52:18 -0500

    Search youtube

--- speechRec.js
+++ speechRec.js
@@ -1,13 +1,1 @@
 
-
-function startListener(){
-  var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
-  recognition.lang = 'en-US';
-  recognition.interimResults = false;
-  recognition.maxAlternatives = 5;
-  recognition.start();
-
-  recognition.onresult = function(event) {
-      console.log('You said: ', event.results[0][0].transcript);
-  };
-}
